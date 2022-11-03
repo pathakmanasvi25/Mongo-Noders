a456c6b6a3c899233acbf925e41fb8853b24f7cf
Manasvi <pathakmanasvi25@gmail.com>
2022-11-04 02:38:33 +0530

    working on databases

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("lo");
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose=require('mongoose');
+const DB="mongodb+srv://pathakmanasvi25:Manasvi%4025@cluster0.zkvzbkm.mongodb.net/hotelDB?retryWrites=true&w=majority";
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    // useCreateIndex:true,
+    useUnifiedTopology:true
+    // useFindAndModify:false
+}).then(()=>{
+    console.log("success");
+}).catch((err) => console.log(err));
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
