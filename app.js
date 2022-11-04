89289e5127d1ba89e68de22eee7b7f67f59f2396
Manasvi <pathakmanasvi25@gmail.com>
2022-11-04 21:03:49 +0530

    commit

--- app.js
+++ app.js
@@ -1,93 +1,132 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const mongoose=require("mongoose");
-const ejs=require("ejs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
 const { application } = require("express");
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine','ejs');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/signupDB",{useNewUrlParser:true});
-var db=mongoose.connection;
+mongoose.connect("mongodb://127.0.0.1/signupDB", { useNewUrlParser: true });
+var db = mongoose.connection;
 
-const userSchema=new mongoose.Schema({
-    name:{ type:String,
-           required:[true,"please fill"]
-         },
-    email:{ type:String,
-            required:[true,"please fill"]
-         },
-    phno: Number,
-    password:String,
-    type:String
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "please fill"]
+    },
+    email: {
+        type: String,
+        required: [true, "please fill"]
+    },
+    phno: {
+        type: Number,
+        required: [true, "please fill"]
+    },
+    password: {
+        type: String,
+        required: [true, "please fill"]
+    },
+    type: {
+        type: String,
+        required: [true, "please fill"]
+    },
+    id: Number
 });
-const User=mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
+const total = 200, gen = 130, special = 70;
 
-app.post("/sign_up",function(req,res){
-   const user=new User({
-         name:req.body.name,
-         email:req.body.email,
-         phno:req.body.phno,
-         password:req.body.password,
-         type:req.body.type,
-   })
-    //  var name=req.body.name;
-    //  var email=req.body.email;
-    //  var phno=req.body.phno;
-    //  var password=req.body.password;
-    //  var type=req.body.type;
+
+const roomSchema = new mongoose.Schema({
     
-    //  var data={
-    //     "name":name,
-    //     "email":email,
-    //     "phno":phno,
-    //     "password":password,
-    //     "type":type
-    //  }
-     
-    //  db.collection("users").insertOne(data,function(err){
-    //     if(err){
-    //         console.log("error");
-    //     }else{
-    //         console.log("success insertion");
-    //     }
-     
-    // })
-   user.save();
-     res.redirect("/login");
+    tor: {
+        type: String, //typeofroom
+        required: [true]
+    },
+    date: {
+        type: Date,
+        required: [true]
+    },
+    bookedBy: userSchema,
+    roomNo: {
+        type: Number,
+        required: [true]
+    }
 });
-app.post("/s",function(req,res){
-    res.redirect("/sign_up");
-});
-app.post("/l",function(req,res){
+
+const Room = mongoose.model("Room", roomSchema);
+
+
+
+
+app.post("/sign_up", function (req, res) {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        phno: req.body.phno,
+        password: req.body.password,
+        type: req.body.type,
+    });
+
+
+    if (user.type === "Monster") {
+        user.id = 3;
+    }
+    else
+        user.id = 4;
+
+    //    if(user.name==="Count Dracula")
+    //    user.id=1;
+    //    if(user.name==="Manager")
+    //    user.id=2;
+    user.save();
     res.redirect("/login");
 });
-app.get("/",function(req,res){
-    
-     res.sendFile(__dirname+"/public/home.html");
+
+
+app.post("/index" , function(req,res)
+{
+
+    const chosenDate = req.body.date;
+    const userid=user.id;
+    const newroom=new Room(
+        {
+            if(userid===3)
+        }
+    )
+})
+app.post("/s", function (req, res) {
+    res.redirect("/sign_up");
 });
-app.get("/sign_up",function(req,res){
-    res.sendFile(__dirname+"/public/signup.html")
+app.post("/l", function (req, res) {
+    res.redirect("/login");
+});
+app.get("/", function (req, res) {
+
+    res.sendFile(__dirname + "/public/home.html");
+});
+app.get("/sign_up", function (req, res) {
+    res.sendFile(__dirname + "/public/signup.html")
 })
-app.get("/login",function(req,res){
-    res.sendFile(__dirname+"/public/login.html")
+app.get("/login", function (req, res) {
+    res.sendFile(__dirname + "/public/login.html")
 })
-app.post("/login",async function(req,res){
-    var pass=req.body.lpassword;
-    var mail=req.body.lemail;
-    const data = await db.collection("users").findOne({email:mail});
+app.post("/login", async function (req, res) {
+    const pass = req.body.lpassword;
+    const mail = req.body.lemail;
+    const data = await db.collection("users").findOne({ email: mail });
     const actualPassword = data.password;
-    const actualName=data.name;
-    if(pass===actualPassword)
-    {
-        res.send("authenticated");
-       
+    const actualName = data.name;
+    const actualId = data.id;
+    if (pass === actualPassword) {
+        res.render("index", { username: actualName, id: actualId });
+
     }
-    else{
-      res.send("wrongPassword");
+    else {
+        res.send("wrongPassword");
     }
-    
+
 });
-app.listen(3000,function(){
-console.log("listening at 3000");
+app.listen(3000, function () {
+    console.log("listening at 3000");
 });
