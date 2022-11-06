90f6e448aec9c160250fa8b662839eeeb96e415f
pandeyg00121 <pranaypandey00121@gmail.com>
2022-11-06 17:25:23 +0530

    styling web pages

--- app.js
+++ app.js
@@ -7,81 +7,81 @@ const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/signupDB",{useNewUrlParser:true});
-var db=mongoose.connection;
+mongoose.connect("mongodb://127.0.0.1/signupDB",{useNewUrlParser:true});
+const db=mongoose.connection;
 
 const userSchema=new mongoose.Schema({
+    
     name:{ type:String,
-           required:[true,"please fill"]
+           required:true,
+           unique:true
          },
     email:{ type:String,
-            required:[true,"please fill"]
+            required:true,
+            unique:true
          },
     phno: Number,
-    password:String,
-    type:String
+    password:{ type:String,
+               required:true,
+             },
+    cpassword:{ type:String,
+                required:true,
+              },         
+    type:String,
+    id:Number
 });
 const User=mongoose.model("User",userSchema);
 
+app.get("/",function(req,res){
+    
+    res.sendFile(__dirname+"/public/home.html");
+});
 app.post("/sign_up",function(req,res){
-   const user=new User({
+   const pass=req.body.password;
+  const cpass=req.body.cpassword;
+  if(pass===cpass){
+    const user=new User({
          name:req.body.name,
          email:req.body.email,
          phno:req.body.phno,
-         password:req.body.password,
+         password:pass,
          type:req.body.type,
-   })
-    //  var name=req.body.name;
-    //  var email=req.body.email;
-    //  var phno=req.body.phno;
-    //  var password=req.body.password;
-    //  var type=req.body.type;
-    
-    //  var data={
-    //     "name":name,
-    //     "email":email,
-    //     "phno":phno,
-    //     "password":password,
-    //     "type":type
-    //  }
-     
-    //  db.collection("users").insertOne(data,function(err){
-    //     if(err){
-    //         console.log("error");
-    //     }else{
-    //         console.log("success insertion");
-    //     }
-     
-    // })
+         cpassword:cpass
+   });
    user.save();
-     res.redirect("/login");
-});
+   if(user.type==="monster"){
+    user.id=3
+   }else{
+    user.id=4;
+   }
+      res.redirect("/");
+ }
+
+     else{
+           res.write("<h1>password did not match</h1>");
+     }
+ });
 app.post("/s",function(req,res){
-    res.redirect("/sign_up");
-});
-app.post("/l",function(req,res){
-    res.redirect("/login");
-});
-app.get("/",function(req,res){
-    
-     res.sendFile(__dirname+"/public/home.html");
-});
-app.get("/sign_up",function(req,res){
-    res.sendFile(__dirname+"/public/signup.html")
-})
-app.get("/login",function(req,res){
     res.sendFile(__dirname+"/public/login.html")
-})
-app.post("/login",async function(req,res){
-    var pass=req.body.lpassword;
-    var mail=req.body.lemail;
+});
+
+
+
+app.post("/login", async function(req,res){
+    const pass=req.body.lpassword;
+    const mail=req.body.lemail;
     const data = await db.collection("users").findOne({email:mail});
+    if(data==null){
+        res.write("invalid user")
+    }
+ 
     const actualPassword = data.password;
     const actualName=data.name;
+    const actualId=data.id;
     if(pass===actualPassword)
     {
-        res.send("authenticated");
-       
+        res.render("index",{userName: actualName,userId: actualId});
+         
     }
     else{
       res.send("wrongPassword");
